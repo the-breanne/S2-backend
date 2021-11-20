3b166209b4ea9cc91117e7da0fd715672e3d2686
Breanne Beutler <breannebeutler@unomaha.edu>
2021-11-19 19:45:58 -0600

    Update serializers.py

--- efs_rest/portfolio/serializers.py
+++ efs_rest/portfolio/serializers.py
@@ -5,7 +5,7 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
 
     class Meta:
-            model = Customer
+            model = Todo
 
             fields = ('task_number', 'task_name', 'task_des', 'due_date')
 
